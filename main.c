c6d886819ec666c4cb018203a9940c0c4f5d11cb
Awais Amjad <awaisamjad08@gmail.com>
2024-07-09 18:40:02 +0100

    bullet code not working

--- main.c
+++ main.c
@@ -3,28 +3,42 @@
 #define SCREEN_WIDTH 960
 #define SCREEN_HEIGHT 540
 
+typedef struct {
+  int x_pos;
+  int y_pos;
+  Texture2D texture;
+} BULLET;
 
-void drawShip(Texture2D ship) {
+void draw_ship(Texture2D ship) {
   int offset = 40;
   if (GetMouseX() < 0) {
     DrawTexture(ship, offset, SCREEN_HEIGHT - 50, WHITE);
-  } 
-  else if (GetMouseX() > SCREEN_WIDTH - offset) {
+  } else if (GetMouseX() > SCREEN_WIDTH - offset) {
     DrawTexture(ship, SCREEN_WIDTH - offset, SCREEN_HEIGHT - 50, WHITE);
-  } 
-  else {
+  } else {
     DrawTexture(ship, GetMouseX(), SCREEN_HEIGHT - 50, WHITE);
   }
 }
 
-//~ function that has all the code/functions that are called before the game and dont have returns
+void shoot_bullet(BULLET bullet, int speed) {
+  // SHould start at ship position and move forward
+  // For 2 dimension movement there should be a POS struct and BULLET should use
+  // that
+  for (int i; i < SCREEN_HEIGHT; i++) {
+    DrawTexture(bullet.texture, bullet.x_pos, bullet.y_pos + (i * speed),
+                WHITE);
+  }
+}
+
+//~ function that has all the code/functions that are called before the game and
+// dont have returns
 void Init() {
   InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Space Invaders");
   SetTargetFPS(60);
   HideCursor();
 }
 
-void UnloadTextures(Texture2D* textures, int length) {
+void UnloadTextures(Texture2D *textures, int length) {
   for (int i = 0; i < length; i++) {
     UnloadTexture(textures[i]);
   }
@@ -35,28 +49,31 @@ int main(void) {
   Texture ship = LoadTexture("assets/ship_outline.png");
   Texture background = LoadTexture("assets/background.png");
 
-  Texture2D textures[] = {
-    ship,
-    background
-  };
-  int textures_length = sizeof(textures)/sizeof(textures[0]);
+  Texture2D textures[] = {ship, background};
+  int textures_length = sizeof(textures) / sizeof(textures[0]);
 
+  BULLET bullet = {1, 1, LoadTexture("assets/bullet.png")};
   while (!WindowShouldClose()) {
     BeginDrawing();
 
-    if (IsMouseButtonPressed(0)){
-      printf("Mouse Button is being Pressed!!!!!!!!!!!!!!!!!!!");
+    // if (IsMouseButtonPressed(0)) {
+    // shoot_bullet(bullet, 1);
+    //}
+    int speed = 5;
+    for (int i; i < SCREEN_HEIGHT; i++) {
+      DrawTexture(bullet.texture, bullet.x_pos, bullet.y_pos + (i * speed),
+                  WHITE);
     }
     DrawTexture(background, 0, 0, WHITE);
     ClearBackground(RAYWHITE);
 
-    drawShip(ship);
+    draw_ship(ship);
     DrawCircle(100, 100, 30, BLUE);
     EndDrawing();
   }
-  
+
   // Freeing the Textures
-  UnloadTextures(textures,textures_length );  
+  UnloadTextures(textures, textures_length);
   CloseWindow();
 
   return 0;
